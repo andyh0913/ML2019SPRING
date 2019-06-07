420a0042332d42456d1dda16c8ee5cee495e7d8d
andyh0913 <andyh0913@gmail.com>
2019-06-07 19:55:53 +0800

    strong baseline

--- hw8/compress.py
+++ hw8/compress.py
@@ -1,31 +1,6 @@
+import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Conv2D, MaxPooling2D, Flatten, Softmax, DepthwiseConv2D, BatchNormalization, GlobalAveragePooling2D
-from keras.optimizers import Adam
-from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import ModelCheckpoint
-import csv
-import numpy as np
-
-batch_size = 256
-epochs = 200
-valid_rate = 0.1
-augmentation = True
-
-def load_data(csv_path="./data/train.csv"):
-	print ("Loading training data...")
-	x_train = []
-	y_train = []
-	with open(csv_path, newline='') as csvfile:
-		next(csvfile)
-		rows = csv.reader(csvfile)
-		for row in rows:
-			one_hot = np.zeros(7)
-			one_hot[int(row[0])] = 1
-			y_train.append(one_hot)
-			x_train.append(np.array( row[1].split() ).astype(np.float))
-	x_train = np.reshape(np.array(x_train),[-1,48,48,1])
-	y_train = np.array(y_train)
-	return x_train / 255. , y_train
 
 def build_model():
 	model = Sequential()
@@ -93,37 +68,26 @@ def build_model():
 	model.add(Dropout(0.3))
 	model.add(Dense(7))
 	model.add(Softmax())
-
-	model.summary()
-	opt = Adam(lr=0.005)
-	# opt = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-	model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
-
+	
 	return model
 
-if __name__ == '__main__':
-	x_train, y_train = load_data()
-	print (x_train.shape)
-	model_path = './models/model_best.h5'
+if __name__ == "__main__":
+	# compress
 	model = build_model()
-	checkpoint = ModelCheckpoint(model_path, save_weights_only=True, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-	if augmentation:
-		datagen = ImageDataGenerator(horizontal_flip=True, rotation_range=30, zoom_range=0.2,
-				shear_range=0.2, fill_mode='nearest', validation_split=valid_rate)
-		datagen.fit(x_train)
-		steps_per_epoch = len(x_train)*(1-valid_rate)*8 // batch_size
-		validation_steps = len(x_train)*valid_rate // batch_size
-		model.fit_generator(datagen.flow(x_train, y_train, batch_size, shuffle=True, subset='training'),
-				steps_per_epoch=steps_per_epoch, epochs=epochs,
-				validation_data=datagen.flow(x_train, y_train, batch_size,subset='validation'), 
-				validation_steps=validation_steps,
-				callbacks=[checkpoint])
-	else:
-		model.fit(x_train, y_train,
-				batch_size=batch_size,
-				epochs=epochs,
-				shuffle=True,
-				validation_split=valid_rate, 
-				callbacks=[checkpoint])
+	model.load_weights('./models/model_best.h5')
+	arr = model.get_weights()
+	arr_16 = []
+	for i in range(len(arr)):
+		arr_16.append(arr[i].astype(np.float16))
+	print (arr_16[0])
+	np.savez_compressed('arr_16.npz', f16=arr_16)
 
+	# load weights
+	# arr_16_l = np.load('arr_16.npz')['f16']
+	# arr_32 = []
+	# for i in range(arr_16_l.shape[0]):
+	# 	arr_32.append(arr_16_l[i].astype(np.float32))
 
+	# model = build_model()
+	# model.set_weights(arr_32)
+	# model.summary()
--- hw8/hw8_test.py
+++ hw8/hw8_test.py
@@ -5,15 +5,91 @@ import csv
 import keras
 import pandas as pd
 from keras.models import load_model
+from keras.layers import Dense, Dropout, Activation, Conv2D, MaxPooling2D, Flatten, Softmax, DepthwiseConv2D, BatchNormalization, GlobalAveragePooling2D
+from keras.models import Sequential
+from keras.preprocessing.image import ImageDataGenerator
+
+seed = 312
+np.random.seed(seed)
 
 batch_size = 256
 
-if __name__ == "__main__":
+def build_model():
+	model = Sequential()
+	# (48,48,32)
+	model.add(Conv2D(16, 3, strides=2, padding='same', input_shape=(48,48,1)))
+	model.add(Activation('relu'))
+	model.add(BatchNormalization())
+	# (24,24,32)
+	model.add(DepthwiseConv2D(3, strides=1, padding='same'))
+	model.add(Activation('relu'))
+	model.add(BatchNormalization())
+	model.add(Conv2D(32, 1, strides=1, padding='same'))
+	model.add(Activation('relu'))
+	model.add(BatchNormalization())
+	# (24,24,64)
+	model.add(DepthwiseConv2D(3, strides=2, padding='same'))
+	model.add(Activation('relu'))
+	model.add(BatchNormalization())
+	model.add(Conv2D(32, 1, strides=1, padding='same'))
+	model.add(Activation('relu'))
+	model.add(BatchNormalization())
+	# (12,12,128)
+	model.add(DepthwiseConv2D(3, strides=1, padding='same'))
+	model.add(Activation('relu'))
+	model.add(BatchNormalization())
+	model.add(Conv2D(64, 1, strides=1, padding='same'))
+	model.add(Activation('relu'))
+	model.add(BatchNormalization())
+	# (12,12,256)
+	model.add(DepthwiseConv2D(3, strides=2, padding='same'))
+	model.add(Activation('relu'))
+	model.add(BatchNormalization())
+	model.add(Conv2D(128, 1, strides=1, padding='same'))
+	model.add(Activation('relu'))
+	model.add(BatchNormalization())
+	
+	# (6,6,256)
+	model.add(DepthwiseConv2D(3, strides=1, padding='same'))
+	model.add(Activation('relu'))
+	model.add(BatchNormalization())
+	model.add(Conv2D(128, 1, strides=1, padding='same'))
+	model.add(Activation('relu'))
+	model.add(BatchNormalization())
+	
+# 	# (6,6,256)
+# 	model.add(DepthwiseConv2D(3, strides=2, padding='same'))
+# 	model.add(Activation('relu'))
+# 	model.add(BatchNormalization())
+# 	model.add(Conv2D(512, 1, strides=1, padding='same'))
+# 	model.add(Activation('relu'))
+# 	model.add(BatchNormalization())
+	# (3,3,512)
+	# model.add(DepthwiseConv2D(3, strides=1, padding='same'))
+	# model.add(Activation('relu'))
+	# model.add(BatchNormalization())
+	# model.add(Conv2D(512, 1, strides=1, padding='same'))
+	# model.add(Activation('relu'))
+	# model.add(BatchNormalization())
+	# (3,3,512)
+	model.add(GlobalAveragePooling2D())
+	# 
+	# model.add(Flatten())
+	model.add(Dense(128))
+	model.add(Activation('relu'))
+	model.add(Dropout(0.3))
+	model.add(Dense(7))
+	model.add(Softmax())
 
+	return model
+
+if __name__ == "__main__":
+	augmentation = True
+	ensemble_num = 8
 	# input_path = sys.argv[1]
 	# output_path = sys.argv[2]
 	input_path = "./data/test.csv"
-	output_path = "./result/ans.csv"
+	output_path = "./result/ans_new_c.csv"
 
 	x_test = []
 	with open(input_path, newline='') as csvfile:
@@ -28,20 +104,52 @@ if __name__ == "__main__":
 	x_test = x_test / 255.
 
 	output_list = [["id", "label"]]
-	model = load_model('./models/model_best.h5')
-	model.save_weights('./models/model_best_weights.h5')
-	
-	iterations =  x_test.shape[0]//batch_size
-	predict = []
-	for j in range(iterations):
-		x = x_test[j*batch_size:(j+1)*batch_size]
-		predict += np.argmax(model.predict(x), axis=1).tolist()
-	if iterations*batch_size < x_test.shape[0]:
-		x = x_test[iterations*batch_size:]
-		predict += np.argmax(model.predict(x), axis=1).tolist()
-	for i,test_y in enumerate(predict):
-		output_list.append([i, test_y])
+	model = build_model()
+	# load weights
+	arr_16_l = np.load('arr_16.npz')['f16']
+	arr_32 = []
+	for i in range(arr_16_l.shape[0]):
+		arr_32.append(arr_16_l[i].astype(np.float32))
 
-	print("Generate ans.csv!")
-	output_file = pd.DataFrame(output_list)
-	output_file.to_csv(output_path, index=False, header=False)
+	model.set_weights(arr_32)
+	model.summary()
+# 	model.load_weights('./models/model_best.h5')
+	
+	if augmentation:
+		datagen = ImageDataGenerator(horizontal_flip=True, rotation_range=30, zoom_range=0.2,
+				shear_range=0.2, fill_mode='nearest')
+		steps = len(x_test) // batch_size + 1
+		model.pop() # take away softmax layer
+		predicts = np.zeros((7178,7))
+		for i in range(ensemble_num):
+			seed += 1
+			datagen.fit(x_test, seed=seed)
+			preds_list = []
+			j = 0
+			for x_batch in datagen.flow(x_test, batch_size=batch_size, shuffle=False, seed=seed):
+				j += 1
+				preds = model.predict(x_batch)
+				preds_list.append(preds)
+				if j == steps: break
+			predicts += np.concatenate(preds_list, axis=0)
+		predicts = np.argmax(predicts, axis = -1)
+		output_file = pd.DataFrame(predicts, columns=['label'])
+		new_ids=output_file.index.set_names("id")
+		output_file.index=new_ids
+		output_file.to_csv(output_path, index=True, header=True)
+		print ("Generate ans.csv!")
+	else:
+		iterations =  x_test.shape[0]//batch_size
+		predict = []
+		for j in range(iterations):
+			x = x_test[j*batch_size:(j+1)*batch_size]
+			predict += np.argmax(model.predict(x), axis=1).tolist()
+		if iterations*batch_size < x_test.shape[0]:
+			x = x_test[iterations*batch_size:]
+			predict += np.argmax(model.predict(x), axis=1).tolist()
+		for i,test_y in enumerate(predict):
+			output_list.append([i, test_y])
+
+		print("Generate ans.csv!")
+		output_file = pd.DataFrame(output_list)
+		output_file.to_csv(output_path, index=False, header=False)
